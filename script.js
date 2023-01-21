a47d8cc4e9c10c8cc735854e76cdbbca0cbb8246
Erick Tapia <ericktapiazuares@gmail.com>
2023-01-21 12:44:09 -0300

    front end listo

---
+++ script.js
@@ -0,0 +1,36 @@
+function encriptar() {
+
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase()
+    //i = para que tome mayusculas y minusculas
+    //m = para que tome multiples lineas
+    //g = tiene en cuenta toda la oración
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "over");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+
+function desencriptar() {
+
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase()
+    //i = para que tome mayusculas y minusculas
+    //m = para que tome multiples lineas
+    //g = tiene en cuenta toda la oración
+    var textoEncriptado = frase.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/over/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+
+function copiar() {
+    var contenido = document.querySelector("#textoDesencriptado");
+    contenido.select();
+    document.execCommand("copy");
+
+}
